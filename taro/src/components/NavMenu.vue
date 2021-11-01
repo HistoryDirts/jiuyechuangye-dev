2ddc32f2e1e582db150d4fba2d24ba2237d0f0f3
Kasper-Heckmatyar <80201304+Kasper-Heckmatyar@users.noreply.github.com>
2021-11-01 15:11:55 +0800

    Add files via upload

---
+++ taro/src/components/NavMenu.vue
@@ -0,0 +1,80 @@
+
+<template>
+    <div style="width: 412px; position:absolute; bottom:0%">
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="onClick"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            >
+            <el-menu-item index="pages/home/home" style="width: 103px; float:left"><i class="el-icon-menu"></i>首页</el-menu-item>
+            <el-menu-item index="pages/discover/discover" style="width: 103px; float:left"><i class="el-icon-s-promotion"></i>发现</el-menu-item>
+            <el-menu-item index="pages/column/column" style="width: 103px; float:left"><i class="el-icon-s-comment"></i>广场</el-menu-item>
+            <el-menu-item index="pages/user/user" style="width: 103px; float:left"><i class="el-icon-user-solid"></i>我的</el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+
+<script>
+import Taro from '@tarojs/taro';
+    export default {
+        name:'NavMenu',
+        data() {
+            return {
+                activeIndex: "pages/home/home"
+            }
+        },
+        // beforeMount(){
+        //     this.activeIndex = Taro.navigateTo.url
+        // },
+        methods: {
+            onClick(key, keyPath) {
+                console.log(typeof(key), keyPath)
+                this.activeIndex = key
+                console.log(this.activeIndex)
+                if(key == "pages/home/home"){
+                    Taro.navigateTo({
+                    url:'pages/home/home'
+                })
+                this.$message({
+                    type: "success",
+                    message: '欢迎来到首页！'
+                })
+                }else if(key == "pages/discover/discover"){
+                    Taro.navigateTo({
+                    url:'pages/discover/discover'
+                })
+                this.$message({
+                    type: "success",
+                    message: '欢迎来到论坛！'
+                })
+                }else if(key == "pages/column/column"){
+                    Taro.navigateTo({
+                    url:'pages/column/column'
+                })
+                this.$message({
+                    type: "success",
+                    message: '欢迎来到广场！'
+                })
+                }else{
+                    Taro.navigateTo({
+                    url:'pages/user/user'
+                })
+                this.$message({
+                    type: "success",
+                    message: '欢迎来到个人主页！'
+                })
+                }
+            },
+        },
+        
+  }
+</script>
+
+<style>
+
+</style>
